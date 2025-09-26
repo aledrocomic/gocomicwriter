e7cdcd419dd189039bac2a3ff25dcf89c9ecd2cb
Alexander Drost <alexander.drost@princess.pink>
2025-09-26 20:21:13 +0200

    0.6.0-dev

--- internal/version/version.go
+++ internal/version/version.go
@@ -9,7 +9,7 @@
 package version
 
 // Version is the current application version. Update this before releases.
-const Version = "0.5.0-dev"
+const Version = "0.6.0-dev"
 
 // String returns the version string.
 func String() string { return Version }
